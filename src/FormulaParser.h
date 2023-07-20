8458d517bcb04961949e3c30a245702730dfd3bf
CrazyOrange <crazydfls@gmail.com>
2023-07-20 23:51:07 +0200

    Reenabled defines to disable some exprtk features in order to reduce compile time

--- src/FormulaParser.h
+++ src/FormulaParser.h
@@ -1,17 +1,17 @@
 ﻿#pragma once
 
-//#define exprtk_disable_cardinal_pow_optimisation
-//#define exprtk_disable_comments
-//#define exprtk_disable_break_continue
-//#define exprtk_disable_sc_andor
-//#define exprtk_disable_return_statement
-//#define exprtk_disable_enhanced_features
-//#define exprtk_disable_string_capabilities
-//#define exprtk_disable_superscalar_unroll
-//#define exprtk_disable_rtl_io
-//#define exprtk_disable_rtl_io_file
-//#define exprtk_disable_rtl_vecops
-//#define exprtk_disable_caseinsensitivity
+#define exprtk_disable_cardinal_pow_optimisation
+#define exprtk_disable_comments
+#define exprtk_disable_break_continue
+#define exprtk_disable_sc_andor
+#define exprtk_disable_return_statement
+#define exprtk_disable_enhanced_features
+#define exprtk_disable_string_capabilities
+#define exprtk_disable_superscalar_unroll
+#define exprtk_disable_rtl_io
+#define exprtk_disable_rtl_io_file
+#define exprtk_disable_rtl_vecops
+#define exprtk_disable_caseinsensitivity
 
 #include "..\libs\exprtk\exprtk.hpp"
 
